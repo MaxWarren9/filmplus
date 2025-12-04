ac87c71d7521f8c741fdcb08ee6f495f7e4bd715
MaxWarren9 <maximvoinoff@gmail.com>
2025-12-04 12:09:27 +0300

    optimised migration

--- src/main/resources/db/changelog/20251911_0000_refactored_user_film_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS filmplus.film
-(
-    id          SERIAL PRIMARY KEY,
-    name        VARCHAR NOT NULL,
-    description VARCHAR NOT NULL,
-    release     DATE    NOT NULL,
-    duration    BIGINT  NOT NULL,
-    genres      VARCHAR NOT NULL
-);
